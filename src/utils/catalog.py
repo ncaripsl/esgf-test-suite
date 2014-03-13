4f1f1c5b843f95b6571f1b7522ef781649fc4d61
Nicolas Carenton <nicolas.carenton@ipsl.jussieu.fr>
2014-03-13 15:41:50 +0100

    201403131541

--- src/utils/catalog.py
+++ src/utils/catalog.py
@@ -1,3 +1,4 @@
+import re
 import urllib2
 from lxml import etree
 import multiprocessing
@@ -16,26 +17,45 @@ class ThreddsUtils(object):
 		global out_queue
 		out_queue = multiprocessing.Queue()
 
+		global catalog_ns
+		catalog_ns = '{http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0}'
+
 		self.config = config.read_config()
 		self.data_node = self.config['nodes']['data_node']
 
 	def chunk_it(self, seq, num):
         	avg = len(seq) / float(num)
-        	out = []
+        	res = []
         	last = 0.0
 
         	while last < len(seq):
-                	out.append(seq[int(last):int(last + avg)])
+                	res.append(seq[int(last):int(last + avg)])
                 	last += avg
 
-        	return out
+        	return res
+
+	def get_dataset_services(self, dataset, services_def):
+		ds_services = []
+
+		# Collecting available services from <serviceName>some_service</serviceName>
+		for sv in dataset.iterchildren(tag=catalog_ns + 'serviceName'):
+			ds_services.append(services_def[sv.text])
+		# Collecting available services from <access serviceName="some_service">
+		for acc in dataset.iterchildren(tag=catalog_ns + 'access'):
+			try:
+				ds_services.append(services_def[acc.get('serviceName')])
+                        except:
+                                continue
+
+		return ds_services
+
 
 	def get_dataset_size(self, dataset):
 		# Set aggregations size to inf
 		if "aggregation" in dataset.get('urlPath'):
 			size = float('inf')
 		else:
-			for si in dataset.iterchildren(tag='{http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0}dataSize'):
+			for si in dataset.iterchildren(tag=catalog_ns + 'dataSize'):
 				units = si.get('units')
 				if units == 'Kbytes':
                         		size = float(si.text) / 1024
@@ -45,53 +65,49 @@ class ThreddsUtils(object):
                                 	size = float(si.text) * 1024
                         	else:
                         		size = float('inf')
+
 		return size
 
+	def get_datasets_list(self, data, services_def):
+		datasets_list = []
 
-	def get_dataset_list(self, data, services_def):
-		dataset_list = []
-		# Parsing datasets
-		doc = etree.iterparse(StringIO(data), tag='{http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0}dataset')
-		for events, ds in doc:
-			
-			# Only if dataset is requestable
+		# Parsing datasets XML document
+		for events, ds in etree.iterparse(StringIO(data), tag=catalog_ns + 'dataset'):
+			# Only interested in datasets which have an URL
 			if ds.get('urlPath'):
+				# Building dataset entry with URL, size and available services
 				dataset = []
 				dataset.append(ds.get('urlPath'))
-
                                 dataset.append(self.get_dataset_size(ds))
+                                dataset.append(self.get_dataset_services(ds, services_def))
+                                datasets_list.append(dataset)
 
-                                ds_services = []
-                                for sv in ds.iterchildren(tag='{http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0}serviceName'):
-                                	ds_services.append(services_def[sv.text])
-                                for acc in ds.iterchildren(tag='{http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0}access'):
-                                	try:
-                                        	ds_services.append(services_def[acc.get('serviceName')])
-                                        except:
-                                        	pass
-                                dataset.append(ds_services)
-                                dataset_list.append(dataset)
-		return dataset_list
-	
+		return datasets_list
 
-	def get_services_def(self, data):
+	def get_services_definition(self, data):
 		services_def = {}
-		doc = etree.iterparse(StringIO(data), tag='{http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0}service')
-		for events, sv in doc:
+
+		for events, sv in etree.iterparse(StringIO(data), tag=catalog_ns + 'service'):
                 	if sv.get('serviceType') != 'Compound':
                         	services_def.update({sv.get('name'):sv.get('serviceType')})
+
 		return services_def
 
 	def worker(self, catalogrefs):
+		datasets_list = []
+
 		for cr in catalogrefs:
 			try:
 				content = urllib2.urlopen(cr)
 				data = content.read()
 			except:
-				continue	
-			services_def = self.get_services_def(data)
-			dataset_list = self.get_dataset_list(data, services_def)
-		return dataset_list
+				continue
+			# Parsing services definition
+			services_def = self.get_services_definition(data)
+			# Parsing datasets
+			datasets_list = self.get_datasets_list(data, services_def)
+
+		return datasets_list
 
 	def queue_manager(self):
 		for item in iter(in_queue.get, None):
@@ -100,8 +116,9 @@ class ThreddsUtils(object):
 		in_queue.task_done()
 
 	def map_processes(self, chunks):
-		# Starting nb_chunk processes calling the queue manager
 		processes = []
+
+		# Starting nb_chunk processes calling the queue manager
 		for i in chunks:
 			processes.append(multiprocessing.Process(target=self.queue_manager))
 			processes[-1].daemon = True
@@ -111,7 +128,7 @@ class ThreddsUtils(object):
 		for cr in chunks:
 			in_queue.put(cr)
 
-		# Waiting for everything to be processed
+		# Waiting for every chunk to be processed
 		in_queue.join()
 
 		# Feeding the input queue with None to be sure
@@ -119,40 +136,57 @@ class ThreddsUtils(object):
 			in_queue.put(None)
 
 		# Collecting results from output queue
-		reslist = []
+		datasets_list = []
 		for p in processes:
-			reslist.extend(out_queue.get())
+			datasets_list.extend(out_queue.get())
 
-		return reslist
+		return datasets_list
 
-	def get_catalogrefs(self):
-        	url = "http://{0}/thredds/esgcet/catalog.xml".format(self.data_node);
-        	content = urllib2.urlopen(url)
+	def get_catalogrefs(self, projects):
+		catalogrefs = []
+
+		for proj_url in projects:
+        		content = urllib2.urlopen(proj_url)
 		
-		# Parsing catalogRef xml entries
-        	doc = etree.iterparse(content, events=('end',), tag='{http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0}catalogRef')
-        	catalogrefs = []
-        	for event, cr in doc:
-			# Keeping monthly ones only 
-        		path = cr.get('{http://www.w3.org/1999/xlink}href')
-        		if ".mon." in path:
-        			catalogrefs.append("http://{0}/thredds/esgcet/{1}".format(self.data_node, path))
+			# Parsing catalogRef xml entries
+        		for event, cr in etree.iterparse(content, events=('end',), tag=catalog_ns + 'catalogRef'):
+				# Only interested in monthly datasets
+        			path = cr.get('{http://www.w3.org/1999/xlink}href')
+        			if ".mon." in path:
+					catalogrefs.append(re.sub('catalog.xml', '', proj_url) + path)
+
         	return catalogrefs
+
+	def get_projects(self):
+		main_url = "http://{0}/thredds/catalog.xml".format(self.data_node);
+		content = urllib2.urlopen(main_url)
+
+		projects = []
+                for event, cr in etree.iterparse(content, events=('end',), tag=catalog_ns + 'catalogRef'):
+			path = cr.get('{http://www.w3.org/1999/xlink}href')
+			# Exclude DatasetScans
+			if 'thredds' not in path:
+				projects.append(re.sub('catalog.xml', '', main_url) + path)
+
+                return projects
 
 
 	def get_endpoints(self):
 		# Determining number of processes and chunks
 		nb_chunks = multiprocessing.cpu_count() * 16
 
-		# Getting and chunking catalogrefs href links from http://my-data-node/thredds/esgcet/catalog.xml
-		catalogrefs = self.get_catalogrefs()
+		# Getting projects href links from main catalog (http://data_node/thredds/catalog.xml)
+		projects = self.get_projects()
+		
+		# Getting and chunking catalogrefs href links from project catalogs (ex: http://data_node/thredds/geomip/catalog.xml)
+		catalogrefs = self.get_catalogrefs(projects)
 		chunked_catalogrefs = self.chunk_it(catalogrefs, nb_chunks)
 		
 		# Starting multiprocessed work
-		res = self.map_processes(chunked_catalogrefs)
+		endpoints = self.map_processes(chunked_catalogrefs)
 
-		res = [i for i in res if 'HTTPServer' in i[2]]
-		print min(res,key=itemgetter(1))
+		endpoints = [i for i in endpoints if 'HTTPServer' in i[2]]
+		print min(endpoints,key=itemgetter(1))
 
 def test_thredds():
         tu = ThreddsUtils()
