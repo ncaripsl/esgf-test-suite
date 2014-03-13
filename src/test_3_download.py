1cafa7b5840b345a32013aebaff2cce3521a03fd
Nicolas Carenton <nicolas.carenton@ipsl.jussieu.fr>
2014-03-13 18:58:44 +0100

    201403131858

--- src/test_3_download.py
+++ src/test_3_download.py
@@ -1,16 +1,24 @@
 import os
 import shutil
 import subprocess
+from operator import itemgetter
 
 import utils.authentication as auth
 import utils.configuration as config
+import utils.catalog as cat
 
 
 
 def setup_module():
 	global _mu
+	global _tu
+
+	global endpoints
 
 	_mu = auth.MyProxyUtils()
+	_tu = cat.ThreddsUtils()
+	endpoints = _tu.get_endpoints()	
+
 	_mu.get_trustroots()
 	_mu.get_credentials()
 
@@ -27,11 +35,21 @@ class TestGridFtp(object):
 		os.environ['X509_CERT_DIR'] = os.path.expanduser("~/.esg/certificates")
 
 	def test_globus_url_copy(self):
-		self.file = self.config['files']['gridftp_url']
-		subprocess.check_call(["globus-url-copy", "-b", self.file, "/tmp/dest_file.nc" ])		
+		#self.file = self.config['files']['gridftp_url']
+		gridftp_endpoints = [i for i in endpoints if 'GridFTP' in i[2]]
+		path = min(gridftp_endpoints,key=itemgetter(1))[0]
+		url = "gsiftp://{0}:2811//{1}".format(self.config['nodes']['data_node'], path)
+		a = subprocess.check_call(["globus-url-copy", "-b", url, "/tmp/dest_file.nc" ])
+		print a
 
 	@classmethod
 	def teardown_class(self):
                 # Delete downloaded file
                 if os.path.exists("/tmp/dest_file.nc"):
                         os.remove("/tmp/dest_file.nc")	
+
+#class TestHttp(object):
+#	def test_wget_download(self):
+#		http_endpoint = [i for i in endpoints if 'HTTPServer' in i[2]]
+#                print min(http_endpoint,key=itemgetter(1))
+
