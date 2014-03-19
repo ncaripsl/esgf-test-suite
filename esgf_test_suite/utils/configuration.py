a5121edac396e55455041dd3418084fa0e6967a2
Nicolas Carenton <nicolas.carenton@ipsl.jussieu.fr>
2014-03-19 17:44:41 +0100

    201403191744

--- esgf_test_suite/utils/configuration.py
+++ esgf_test_suite/utils/configuration.py
@@ -1,20 +1,18 @@
 import os
 import sys
 import site
+import inspect
 import subprocess
 
 from ConfigParser import SafeConfigParser
 
 def read_config():
-	#filePath = os.getcwd()+"/configuration.ini"
-	#filePath = os.path.expanduser("~") + "/code//esgf-test-suite/esgf-test-suite/configuration.ini"
-	usersitepackages = subprocess.Popen(['python', '-m', 'site', '--user-site'], stdout=subprocess.PIPE).communicate()[0]#check_output(["python", "-m", "site", "--user-site"])
-	usersitepackages = usersitepackages[:-2]
-	filePath = "/usr/lib/python2.6/site-packages/esgf_test_suite-0.1-py2.6.egg/esgf_test_suite/configuration.ini"
+	modulePath = os.path.abspath(inspect.getsourcefile(lambda _: None))
+	configPath = modulePath.rsplit('/', 2)[0]+'/configuration.ini'
 
 	parser = SafeConfigParser()
 
-	parser.readfp(open(filePath))
+	parser.readfp(open(configPath))
 	config = dict((section, dict((option, parser.get(section, option))
                              for option in parser.options(section)))
               for section in parser.sections())	
