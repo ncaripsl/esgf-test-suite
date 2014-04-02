482e1cc9be22ed332dcd82bb4277693d2a3d1f5a
Nicolas Carenton <nicolas.carenton@ipsl.jussieu.fr>
2014-04-02 15:07:38 +0200

    esgf

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(name='esgf_test_suite',
-      version='0.3',
+      version='0.4',
       description='Nose scripts for ESGF integration test and validation',
-      url='http://github.com/ncaripsl/esgf-test-suite',
+      url='http://github.com/ncaripsl/esgf_test_suite',
       author='Nicolas Carenton',
       author_email='nicolas.carenton@ipsl.jussieu.fr',
       license='IPSL',
       packages=['esgf_test_suite'],
       install_requires=[
           'nose',
-          'pyOpenSSL',
+          'pyOpenSSL==0.10',
 	  'MyProxyClient',
 	  'requests',
 	  'lxml',
