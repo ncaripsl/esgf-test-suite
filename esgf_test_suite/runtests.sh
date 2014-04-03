c5a495784cb1f3baa17af79e3bfb5f1130f26446
Nicolas Carenton <nicolas.carenton@ipsl.jussieu.fr>
2014-04-03 15:19:44 +0200

    esgf

--- esgf_test_suite/runtests.sh
+++ esgf_test_suite/runtests.sh
@@ -29,6 +29,6 @@ export PYTHONPATH=$PYTHONPATH:$PWD
 script_path="$(readlink -f ${BASH_SOURCE[0]})"
 dir_path="$(dirname $script_path)"
 
-nosetests $dir_path -v --exe --nocapture
+nosetests $dir_path -v --exe --nocapture --nologcapture
 
 exit 0
